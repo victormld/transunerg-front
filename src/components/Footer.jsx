5b839a77228be56bfd39bfb4c4197fb0c543f786
GGene29 <sumozacalcurian@gmail.com>
2024-02-20 02:10:34 -0400

    [FIX]-Estilos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Nav } from 'reactstrap';
 
 const Footer = () => {
     return (
-        <Nav className='bg-primary text-white fixed-bottom'>
+        <Nav className='bg-fondos text-white fixed-bottom'>
             <p id="barraInferior" className='container-fluid text-center'>Traductores e Interpretes / Equipo de Estudiantes 2024</p>
         </Nav>
     );
