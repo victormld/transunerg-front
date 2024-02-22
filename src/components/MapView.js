7eaef1494c78e799cb9e24a4c3354d1e29c88bdc
Victor Maldonado <victoramaldonadog@gmail.com>
2024-02-22 10:38:05 -0400

    ubicaciones

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -16,8 +16,24 @@ const MapView = ()=>{
     return <MapContainer center={{ lat: "9.892970652555023", lng: "-67.38683018820838" }} zoom={15}>
         <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={{ lat: "9.893575787463098", lng: "-67.38627598911934"}}/>
+        />
+        {/* parada agronomia */}
+        <Marker position={{ lat: "9.900806121609884", lng: "-67.39686540049382"}}/>
+        {/* parada informatica */}
+        <Marker position={{ lat: "9.897274079749067", lng: "-67.39139727353472"}}/>
+        {/* {parada medicina} */}
+        <Marker position={{ lat: "9.893575787463098", lng: "-67.38627598911934"}}/>
+        {/* vista hermosa */}
+        <Marker position={{ lat: "9.912694097984248", lng: "-67.36143756523738"}}/>
+        {/* villa olimpica */}
+        <Marker position={{ lat: "9.917578179998147", lng: "-67.36836756537615"}}/>
+        {/* Banco Venezuela */}
+        <Marker position={{ lat: "9.910548952437733", lng: "-67.3589334170435"}}/>
+        {/* puerta negra*/}
+        <Marker position={{ lat: "9.90353261402284", lng: "-67.37217688365276"}}/>
+        {/* circuito */}
+        <Marker position={{ lat: "9.899999124470032", lng: "-67.37974460539726"}}/>
+        
     </MapContainer>
 }
 
--- src/components/Markers.js
+++ src/components/Markers.js
@@ -5,6 +5,7 @@ import { IconLocation } from "./IconLocation";
 
 const Markers = ()=>{
     return <Marker position={{ lat: "9.893575787463098", lng: "-67.38627598911934"}} icon={IconLocation} />
+    
 }
 
 export default Markers
